8117b1437995ed878f399b57c693f6e04c7c6e0d
Harold Treen <haroldtreen@gmail.com>
2017-01-16 16:27:55 -0500

    Linting + Editor config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "import/no-extraneous-dependencies": 0,
+      "no-bitwise": 0,
+      "no-console": 0,
+    }
+};
