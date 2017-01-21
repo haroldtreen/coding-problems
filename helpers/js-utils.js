3c4a9ebbd699fde120bc31463b1ddf4d201f2307
Harold Treen <haroldtreen@gmail.com>
2017-01-21 16:18:50 -0500

    Add helpers for loading inputs

---
+++ helpers/js-utils.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+class Utils {
+  static loadInputLines(filename, i) {
+    const input = filename.replace(/\.js/, `-input-${i}.txt`);
+    return fs.readFileSync(input).toString().split('\n').filter(l => l !== '');
+  }
+
+  static loadOutputLines(filename, i) {
+    const output = filename.replace(/\.js/, `-output-${i}.txt`);
+    return fs.readFileSync(output).toString().split('\n').filter(l => l !== '');
+  }
+}
+
+module.exports = Utils;
